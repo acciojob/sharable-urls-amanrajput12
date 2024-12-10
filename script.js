13205fe01897018932762b3d8abb3c5fc9e2f2e2
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2024-12-10 23:02:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+
+let inputname = document.getElementById("name");
+let url = document.getElementById("url");
+let preval = document.getElementById("url")
+const inputyear =document.getElementById("year");
+const submit = document.getElementById("button");
+submit.addEventListener("click",()=>{
+	 
+	
+ if(inputname.value==""){
+	    url.innerHTML+=`?year=${inputyear.value}`
+}
+	else if(inputyear.value==""){
+		url.innerHTML+=`?name=${inputname.value}`
+}
+else {
+	url.innerHTML+=`?name=${inputname.value}&year=${inputyear.value}`
+}	
+})
